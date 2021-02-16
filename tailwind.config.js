e252771c9621702a1db587d8dba23c2d53d46574
difaananda40 <difacool40@gmail.com>
2021-02-17 01:38:19 +0800

    Add fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,19 @@ module.exports = {
   purge: ['./src/**/*.{js,jsx,ts,tsx}'],
   darkMode: false, // or 'media' or 'class'
   theme: {
+    screens: {
+      'xs': '414px',
+      'sm': '600px',
+      'md': '900px',
+      'lg': '1124px',
+      'xl': '1265px',
+      '2xl': '1530px'
+    },
+    fontFamily: {
+      sans: ['Source Code Pro', 'sans-serif'],
+      serif: ['Source Serif Pro', 'serif'],
+      mono: ['Source Sans Pro', 'monospace'],
+    },
     extend: {},
   },
   variants: {
