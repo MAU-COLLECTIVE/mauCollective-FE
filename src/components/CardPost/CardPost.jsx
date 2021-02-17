855fdefdf87cfe89d1c0483b7f64d67e3bfefba1
difaananda40 <difacool40@gmail.com>
2021-02-17 23:54:14 +0800

    Add card post component

---
+++ src/components/CardPost/CardPost.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const CardPost = () => (
+  <div className="flex flex-col space-y-4">
+    <div className="w-full h-40 bg-gray-300">Image</div>
+    <h2 className="font-mono font-semibold text-xl">Card Title</h2>
+    <p className="font-light">18.01.2021</p>
+  </div>
+)
+
+export default CardPost
