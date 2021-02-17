346c68d621f93aaabd802cbdfb1d4c6fc7b04b41
difaananda40 <difacool40@gmail.com>
2021-02-18 04:00:32 +0800

    Add artist page (sample)

---
+++ src/pages/artist.jsx
@@ -0,0 +1,150 @@
+import React from 'react'
+import BadgeNumber from 'components/shared/BadgeNumber'
+
+const Artist = () => {
+  return (
+    <div className="min-h-screen max-w-screen p-10 flex flex-col items-start bg-black text-white">
+      <div className="relative px-6 py-4 mb-12">
+        <BadgeNumber number="01" />
+        <a href="#" className="block font-medium uppercase">Close</a>
+      </div>
+      <div className="flex">
+        <div className="xl:w-2/3">
+          <div className="relative px-6 py-4 mb-6">
+            <BadgeNumber number="02" />
+            <a href="#" className="block font-semibold text-3xl uppercase">Joe Karnavian</a>
+          </div>
+          <p className="px-6 font-mono sm:text-lg xl:text-xl text-gray-200 mb-32">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad animi, assumenda nisi, quisquam molestias iste quod ratione officia aut dignissimos, adipisci hic. Ratione, distinctio? Magni optio exercitationem vel molestias suscipit.</p>
+          <div className="lg:hidden flex flex-1 flex-col items-center space-y-8 mb-32">
+            <img src="/img/logo.svg" alt="logo" />
+            <div className="flex space-x-2">
+              <a href="#fb"><img src="/icons/fb.svg"/></a>
+              <a href="#fb"><img src="/icons/ig.svg"/></a>
+              <a href="#fb"><img src="/icons/youtube.svg"/></a>
+              <a href="#fb"><img src="/icons/spotify.svg"/></a>
+            </div>
+          </div>
+          <div className="flex flex-col md:flex-row md:space-x-16 space-y-16 md:space-y-0 justify-between lg:justify-start">
+            <div className="flex flex-col">
+              <div className="relative px-6 py-4 mb-6">
+                <BadgeNumber number="03" />
+                <a href="#" className="block font-semibold text-3xl uppercase">Music</a>
+              </div>
+              <ul className="px-6">
+                <li className="flex space-x-2 md:space-x-8 font-mono font-light text-lg text-gray-400 border-b border-gray-400 py-6 first:pt-0">
+                  <p>2019</p>
+                  <div className="flex flex-col">
+                    <p>Wean</p>
+                    <p className="text-sm">Live Performance</p>
+                  </div>
+                  <div className="flex flex-col flex-1 items-end">
+                    <p className="text-right">Stream on:</p>
+                    <div className="flex space-x-1">
+                      <img src="/icons/fb.svg" />
+                      <img src="/icons/ig.svg" />
+                    </div>
+                  </div>
+                </li>
+                <li className="flex space-x-2 md:space-x-8 font-mono font-light text-lg text-gray-400 border-b border-gray-400 py-6 first:pt-0">
+                  <p>2019</p>
+                  <div className="flex flex-col">
+                    <p>Wean</p>
+                    <p className="text-sm">Live Performance</p>
+                  </div>
+                  <div className="flex flex-col flex-1 items-end">
+                    <p className="text-right">Stream on:</p>
+                    <div className="flex space-x-1">
+                      <img src="/icons/fb.svg" />
+                      <img src="/icons/ig.svg" />
+                    </div>
+                  </div>
+                </li>
+                <li className="flex space-x-2 md:space-x-8 font-mono font-light text-lg text-gray-400 border-b border-gray-400 py-6 first:pt-0">
+                  <p>2019</p>
+                  <div className="flex flex-col">
+                    <p>Wean</p>
+                    <p className="text-sm">Live Performance</p>
+                  </div>
+                  <div className="flex flex-col flex-1 items-end">
+                    <p className="text-right">Stream on:</p>
+                    <div className="flex space-x-1">
+                      <img src="/icons/fb.svg" />
+                      <img src="/icons/ig.svg" />
+                    </div>
+                  </div>
+                </li>
+                <li className="flex justify-end space-x-8 font-light text-sm text-gray-200 py-6 first:pt-0">
+                  1/2
+                </li>
+              </ul>
+            </div>
+            <div className="flex flex-col">
+              <div className="relative px-6 py-4 mb-6">
+                <BadgeNumber number="04" />
+                <a href="#" className="block font-semibold text-3xl uppercase">Video</a>
+              </div>
+              <ul className="px-6">
+                <li className="flex space-x-2 md:space-x-8 font-mono font-light text-lg text-gray-400 border-b border-gray-400 py-6 first:pt-0">
+                  <p>2019</p>
+                  <div className="flex flex-col">
+                    <p>Wean</p>
+                    <p className="text-sm">Live Performance</p>
+                  </div>
+                  <div className="flex flex-col flex-1 items-end">
+                    <p className="text-right">Stream on:</p>
+                    <div className="flex space-x-1">
+                      <img src="/icons/fb.svg" />
+                      <img src="/icons/ig.svg" />
+                    </div>
+                  </div>
+                </li>
+                <li className="flex space-x-2 md:space-x-8 font-mono font-light text-lg text-gray-400 border-b border-gray-400 py-6 first:pt-0">
+                  <p>2019</p>
+                  <div className="flex flex-col">
+                    <p>Wean</p>
+                    <p className="text-sm">Live Performance</p>
+                  </div>
+                  <div className="flex flex-col flex-1 items-end">
+                    <p className="text-right">Stream on:</p>
+                    <div className="flex space-x-1">
+                      <img src="/icons/fb.svg" />
+                      <img src="/icons/ig.svg" />
+                    </div>
+                  </div>
+                </li>
+                <li className="flex space-x-2 md:space-x-8 font-mono font-light text-lg text-gray-400 border-b border-gray-400 py-6 first:pt-0">
+                  <p>2019</p>
+                  <div className="flex flex-col">
+                    <p>Wean</p>
+                    <p className="text-sm">Live Performance</p>
+                  </div>
+                  <div className="flex flex-col flex-1 items-end">
+                    <p className="text-right">Stream on:</p>
+                    <div className="flex space-x-1">
+                      <img src="/icons/fb.svg" />
+                      <img src="/icons/ig.svg" />
+                    </div>
+                  </div>
+                </li>
+                <li className="flex justify-end space-x-8 font-light text-sm text-gray-200 py-6 first:pt-0">
+                  1/2
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+        <div className="hidden lg:flex flex-1 flex-col items-center space-y-8">
+          <img src="/img/logo.svg" alt="logo" />
+          <div className="flex space-x-2">
+            <a href="#fb"><img src="/icons/fb.svg"/></a>
+            <a href="#fb"><img src="/icons/ig.svg"/></a>
+            <a href="#fb"><img src="/icons/youtube.svg"/></a>
+            <a href="#fb"><img src="/icons/spotify.svg"/></a>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Artist
