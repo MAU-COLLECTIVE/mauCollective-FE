f1b8f2771eb16eef58ea1e8fb3713a2681459718
difaananda40 <difacool40@gmail.com>
2021-02-18 01:31:03 +0800

    Add responsive

--- src/components/Home/Recent.jsx
+++ src/components/Home/Recent.jsx
@@ -4,25 +4,28 @@ import CardPost from 'components/shared/CardPost'
 
 const RecentNews = () => {
   return (
-    <div className="px-6 py-28 flex flex-col">
-      <a href="#view-all-news" className="font-light uppercase w-full text-right px-6 mb-4">View all news</a>
-      <div className="flex">
-        <div className="relative px-6 py-4 w-1/3">
+    <div className="px-2 lg:px-6 py-28 flex flex-col">
+      <a href="#view-all-news" className="font-light uppercase w-full text-right px-2 lg:px-6 mb-4">View all news</a>
+      <div className="flex flex-col xl:flex-row">
+        <div className="relative px-2 lg:px-6 py-4 w-full xl:w-1/3">
           <BadgeNumber number="02" />
-          <h1 className="font-black text-6xl mb-52 uppercase">MOST RECENT NEWS HEADLINE</h1>
-          <p className="font-mono text-xl text-gray-600 mb-12">Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum adipisci, voluptates excepturi molestias dolorum ducimus soluta cupiditate possimus nobis quam dicta sint illo doloremque pariatur quae, quia aut, officiis animi.</p>
-          <a href="#continue-read" className="font-light uppercase border-b-2 border-black border-solid">Continue reading</a>
+          <h1 className="font-black text-3xl sm:text-4xl md:text-5xl xl:text-6xl mb-8 xl:mb-52 uppercase">MOST RECENT NEWS HEADLINE</h1>
+          <div className="xl:hidden md:block w-full h-96 mb-8 bg-gray-300">Image</div>
+          <div className="flex flex-col md:flex-row xl:flex-col md:space-x-8 items-start justify-start xl:space-x-0 space-y-8 md:space-y-0 xl:space-y-8">
+            <p className="font-mono text-xl text-gray-600">Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum adipisci, voluptates excepturi molestias dolorum ducimus soluta cupiditate possimus nobis quam dicta sint illo doloremque pariatur quae, quia aut, officiis animi.</p>
+            <a href="#continue-read" className="font-light uppercase border-b-2 border-black border-solid whitespace-nowrap">Continue reading</a>
+          </div>
         </div>
-        <div className="px-6 flex-1 space-y-6">
-          <div className="block w-full h-96 bg-gray-300">Image</div>
+        <div className="px-2 lg:px-6 flex-1 space-y-6">
+          <div className="hidden xl:block w-full h-96 bg-gray-300">Image</div>
           <div className="flex space-x-6">
-            <div className="w-1/3">
+            <div className="w-1/2 lg:w-1/3">
               <CardPost />
             </div>
-            <div className="w-1/3">
+            <div className="w-1/2 lg:w-1/3">
               <CardPost />
             </div>
-            <div className="w-1/3">
+            <div className="hidden lg:block lg:w-1/3">
               <CardPost />
             </div>
           </div>
