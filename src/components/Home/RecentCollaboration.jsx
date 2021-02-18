2d35d79a7407128d7242fc43be918b7f5f46c701
difaananda40 <difacool40@gmail.com>
2021-02-19 00:08:51 +0800

    Add example for image slideshow

--- src/components/Home/RecentCollaboration.jsx
+++ src/components/Home/RecentCollaboration.jsx
@@ -1,24 +1,42 @@
-import React from 'react'
-import BadgeNumber from 'components/shared/BadgeNumber'
+import React, { useState } from 'react'
+
+const images = [
+  '/img/avril.jpg',
+  '/img/collab.png'
+];
+
+const data = [
+  {
+    title: 'AVRIL OMO WITH RHYMASTIC & SUBOI IN TET HOLIDAY CAMPAIGN.',
+    desc: 'Avril Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum adipisci, voluptates excepturi molestias dolorum ducimus soluta cupiditate possimus nobis quam dicta sint illo doloremque pariatur quae, quia aut, officiis animi.',
+    img: '/img/avril.jpg'
+  },
+  {
+    title: 'OMO WITH RHYMASTIC & SUBOI IN TET HOLIDAY CAMPAIGN.',
+    desc: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum adipisci, voluptates excepturi molestias dolorum ducimus soluta cupiditate possimus nobis quam dicta sint illo doloremque pariatur quae, quia aut, officiis animi.',
+    img: '/img/collab.png'
+  }
+]
 
 const RecentCollaboration = () => {
+  
+  const [currentIndex, setCurrentIndex] = useState(0);
+
   return (
-    <div className="px-2 lg:px-6 py-28 flex flex-col bg-gray-300">
-      <div className="text-right px-2 lg:px-6 mb-4">
+    <div className="relative px-2 lg:px-6 py-28 flex flex-col text-gray-200">
+      <img className="absolute w-full h-full object-cover -mx-2 lg:-mx-6 -my-28 z-0 transition-all" style={{ filter: 'brightness(75%)' }} src={data?.[currentIndex]?.img} />
+      <div className="text-right px-2 lg:px-6 mb-4 z-10">
         <a href="#view-all-news" className="font-light text-sm md:text-base uppercase">View all <br/>collaborations</a>
       </div>
-      <div className="flex flex-col lg:flex-row px-2 lg:px-6">
+      <div className="flex flex-col lg:flex-row px-2 lg:px-6 z-10">
         <div className="relative w-full lg:w-1/2 xl:w-1/3">
-          <h1 className="font-black text-3xl sm:text-4xl md:text-5xl xl:text-6xl mb-8 xl:mb-28 uppercase">OMO WITH RHYMASTIC & SUBOI IN TET HOLIDAY CAMPAIGN.</h1>
-          <p className="font-mono sm:text-lg xl:text-xl text-gray-600 mb-8">Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum adipisci, voluptates excepturi molestias dolorum ducimus soluta cupiditate possimus nobis quam dicta sint illo doloremque pariatur quae, quia aut, officiis animi.</p>
-          <a href="#continue-read" className="font-light text-sm md:text-base uppercase border-b-2 border-black border-solid whitespace-nowrap">Watch video</a>
+          <h1 className="font-black text-3xl sm:text-4xl md:text-5xl xl:text-6xl mb-8 xl:mb-28 uppercase">{data?.[currentIndex]?.title}</h1>
+          <p className="font-mono sm:text-lg xl:text-xl mb-8">{data?.[currentIndex]?.desc}</p>
+          <a href="#continue-read" className="font-light text-sm md:text-base uppercase border-b-2 border-white border-solid whitespace-nowrap">Watch video</a>
         </div>
         <ul className="flex flex-row mt-28 lg:mt-0 lg:flex-col lg:flex-1 lg:items-end justify-center space-x-4 lg:space-x-0 lg:space-y-4 text-sm md:text-base">
-          <li className="border-b-2 border-black">01</li>
-          <li>02</li>
-          <li>03</li>
-          <li>04</li>
-          <li>05</li>
+          <li onClick={() => setCurrentIndex(0)} className={`cursor-pointer transition-all ${currentIndex == 0 && 'border-b-2 border-white'}`}>01</li>
+          <li onClick={() => setCurrentIndex(1)} className={`cursor-pointer transition-all ${currentIndex == 1 && 'border-b-2 border-white'}`}>02</li>
         </ul>
       </div>
     </div>
