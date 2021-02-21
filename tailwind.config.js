c0eee83a3c5862a7f124de24037472aae8ea93a2
difaananda40 <difacool40@gmail.com>
2021-02-22 01:26:26 +0800

    Add minWidth variant

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,10 @@ module.exports = {
   purge: ['./src/**/*.{js,jsx,ts,tsx}'],
   darkMode: false, // or 'media' or 'class'
   theme: {
+    minWidth: {
+      '0': '0',
+      '3/4': '75%'
+     },
     screens: {
       'xs': '414px',
       'sm': '600px',
