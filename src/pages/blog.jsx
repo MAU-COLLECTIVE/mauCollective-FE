e719c6e1dba8e80623b94dc2bdee040234736e6a
difaananda40 <difacool40@gmail.com>
2021-02-22 02:45:41 +0800

    Update spacing

--- src/pages/blog.jsx
+++ src/pages/blog.jsx
@@ -7,8 +7,8 @@ const BlogPage = () => {
   return (
     <BlogLayout>
       <div className="flex flex-col">
-        <h1 className="font-black text-3xl sm:text-4xl md:text-5xl xl:text-6xl mb-8 xl:mb-28 uppercase">NEWS</h1>
-        <div className="grid grid-cols-2 2xl:grid-cols-3 3xl:grid-cols-4 gap-x-6 gap-y-10">
+        <h1 className="font-black text-3xl sm:text-4xl md:text-5xl xl:text-6xl mb-12 uppercase">NEWS</h1>
+        <div className="grid grid-cols-2 2xl:grid-cols-3 3xl:grid-cols-4 gap-x-6 gap-y-12">
           <CardPost />
           <CardPost />
           <CardPost />
