3db1e5d2ba5afe788912ee02454e336ebe6eccf6
difaananda40 <difacool40@gmail.com>
2021-02-25 02:01:25 +0800

    Add transition when changing slides

--- src/components/Home/RecentCollaboration.jsx
+++ src/components/Home/RecentCollaboration.jsx
@@ -1,26 +1,51 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect, useRef } from 'react'
 import PropTypes from 'prop-types'
+import { TweenMax } from 'gsap/all'
 
 const data = [
   {
+    id: 1,
     title: 'AVRIL OMO WITH RHYMASTIC & SUBOI IN TET HOLIDAY CAMPAIGN.',
     desc: 'Avril Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum adipisci, voluptates excepturi molestias dolorum ducimus soluta cupiditate possimus nobis quam dicta sint illo doloremque pariatur quae, quia aut, officiis animi.',
     img: '/img/avril.jpg'
   },
   {
+    id: 2,
     title: 'OMO WITH RHYMASTIC & SUBOI IN TET HOLIDAY CAMPAIGN.',
     desc: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum adipisci, voluptates excepturi molestias dolorum ducimus soluta cupiditate possimus nobis quam dicta sint illo doloremque pariatur quae, quia aut, officiis animi.',
     img: '/img/collab.png'
   }
 ]
 
+function usePrevious(value) {
+  const ref = useRef();
+  useEffect(() => {
+    ref.current = value;
+  });
+  return ref.current;
+}
+
 const RecentCollaboration = ({ id }) => {
   
   const [currentIndex, setCurrentIndex] = useState(0);
+  const prevCurrentIndex = usePrevious(currentIndex);
+  const imgRef = useRef([]);
+
+  useEffect(() => {
+    TweenMax.to(imgRef.current[prevCurrentIndex], 0.5, {css:{autoAlpha:0}}) 
+    TweenMax.to(imgRef.current[currentIndex], 0.5, {css:{autoAlpha:1}})
+  }, [currentIndex])
 
   return (
     <div id={id} className="min-h-screen relative px-2 lg:px-6 py-28 flex flex-col justify-between text-gray-200">
-      <img className="absolute w-full h-full object-cover -mx-2 lg:-mx-6 -my-28 z-0 transition-all" style={{ filter: 'brightness(75%)' }} src={data?.[currentIndex]?.img} />
+      {data.map((dt, index) => (
+        <img
+          key={dt.id}
+          className="absolute w-full h-full object-cover -mx-2 lg:-mx-6 -my-28 z-0 transition-all invisible"
+          style={{ filter: 'brightness(75%)' }}
+          src={dt?.img}
+          ref={elm => imgRef.current[index] = elm} />
+      ))}
       <div className="text-right px-2 lg:px-6 mb-4 z-10">
         <a href="#view-all-news" className="font-light text-sm md:text-base uppercase">View all <br/>collaborations</a>
       </div>
