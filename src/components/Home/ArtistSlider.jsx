d42741b6bd46d7739aa039fdf50c1c3b807695e0
difaananda40 <difacool40@gmail.com>
2021-03-04 02:02:20 +0800

    Add artist slider

---
+++ src/components/Home/ArtistSlider.jsx
@@ -0,0 +1,137 @@
+import React, { useEffect } from 'react'
+import gsap from 'gsap'
+import { Draggable } from 'gsap/Draggable'
+
+const ArtistSlider = () => {
+
+  useEffect(() => {
+    gsap.defaults({
+      ease: "none"
+    });
+  
+    gsap.registerPlugin(Draggable);
+
+    var picker = document.querySelector(".picker");
+    var cells = document.querySelectorAll(".cell");
+    var proxy = document.createElement("div");
+
+    var cellWidth = 600;
+    if(window.innerWidth > 740) {
+      cellWidth = 600;
+    }
+    else {
+      cellWidth = 400;
+    }
+    var rotation = -90;
+
+    var numCells = cells.length;
+    var cellStep = 1 / numCells;
+    var wrapWidth = cellWidth * numCells;
+
+    var baseTl = gsap.timeline({ paused: true });
+
+    gsap.set(picker, {
+      perspective: 1100,
+      width: wrapWidth - cellWidth
+    });
+
+    for (var i = 0; i < cells.length; i++) {
+      initCell(cells[i], i);
+    }
+
+    var animation = gsap
+      .timeline({ repeat: -1, paused: true })
+      .add(baseTl.tweenFromTo(1, 2))
+      .progress(1);
+
+    var draggable = new Draggable(proxy, {
+      allowContextMenu: true,
+      type: "x",
+      trigger: picker,
+      inertia: true,
+      onDrag: updateProgress,
+      onThrowUpdate: updateProgress,
+      snap: {
+        x: snapX
+      },
+      onThrowComplete: function () {
+        console.log("onThrowComplete");
+        //TODO: animation that inject selected card title
+      },
+      onClick: function() {
+        console.log(this)
+      }
+    });
+
+    function snapX(x) {
+      return Math.round(x / cellWidth) * cellWidth;
+    }
+
+    function updateProgress() {
+      let newProg = this.x / wrapWidth;
+      newProg = newProg - Math.floor(newProg);
+      animation.progress(newProg);
+    }
+
+    function initCell(element, index) {
+      gsap.set(element, {
+        width: cellWidth,
+        scale: 0.9,
+        rotation: rotation,
+        x: -cellWidth
+      });
+
+      var tl = gsap
+        .timeline({ repeat: 1 })
+        .to(element, 1, { x: "+=" + wrapWidth, rotation: -rotation }, 0)
+        .to(element, cellStep, { scale: 1, repeat: 1, yoyo: true }, 0.5 - cellStep);
+
+      baseTl.add(tl, i * -cellStep);
+    }
+  }, [])
+
+  return (
+    <div className="bg-black text-white h-screen relative flex flex-col w-full items-center justify-center overflow-hidden">
+      <div id="picker" className="picker">
+        <div className="cell">
+          <div className="cell-content">
+            <img src="/img/artist.png" className="rounded-full w-full h-full" />
+            <h1 className="text-center font-medium text-lg uppercase mt-8">Dycal</h1>
+          </div>
+        </div>     
+        <div className="cell">
+          <div className="cell-content">
+            <img src="/img/artist.png" className="rounded-full w-full h-full" />
+            <h1 className="text-center font-medium text-lg uppercase mt-8">Dycal</h1>
+          </div>
+        </div>  
+        <div className="cell">
+          <div className="cell-content">
+            <img src="/img/artist.png" className="rounded-full w-full h-full" />
+            <h1 className="text-center font-medium text-lg uppercase mt-8">Dycal</h1>
+          </div>
+        </div>  
+        <div className="cell">
+          <div className="cell-content">
+            <img src="/img/artist.png" className="rounded-full w-full h-full" />
+            <h1 className="text-center font-medium text-lg uppercase mt-8">Dycal</h1>
+          </div>
+        </div>
+        <div className="cell">
+          <div className="cell-content">
+            <img src="/img/artist.png" className="rounded-full w-full h-full" />
+            <h1 className="text-center font-medium text-lg uppercase mt-8">Dycal</h1>
+          </div>
+        </div>
+        <div className="cell">
+          <div className="cell-content">
+            <img src="/img/artist.png" className="rounded-full w-full h-full" />
+            <h1 className="text-center font-medium text-lg uppercase mt-8">Dycal</h1>
+          </div>
+        </div>
+      </div>  
+    </div>
+  )
+}
+
+export default ArtistSlider
--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -2,10 +2,12 @@ import Recent from './Recent'
 import About from './About'
 import RecentCollaboration from './RecentCollaboration'
 import Artists from './Artists'
+import ArtistSlider from './ArtistSlider'
 
 export {
   Recent,
   About,
   RecentCollaboration,
-  Artists
+  Artists,
+  ArtistSlider
 }
