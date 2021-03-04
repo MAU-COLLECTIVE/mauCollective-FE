4caa9a561671594ec0e323257b3eca2adcda6632
difaananda40 <difacool40@gmail.com>
2021-03-05 03:23:59 +0800

    Fix spacing

--- src/components/Home/Recent.jsx
+++ src/components/Home/Recent.jsx
@@ -7,7 +7,7 @@ import AniLink from 'gatsby-plugin-transition-link/AniLink'
 const Recent = ({ id }) => {
   return (
     <div id={id} className="min-h-screen bg-white px-2 lg:px-6 py-28 flex flex-col justify-center">
-      <div className="text-right px-2 lg:px-6 mb-4">
+      <div className="text-right px-2 lg:px-6 mb-2">
         <AniLink
           swipe direction="left"
           top="entry" to="/blog"
@@ -29,7 +29,7 @@ const Recent = ({ id }) => {
                 Continue reading</AniLink>
           </div>
         </div>
-        <div className="px-2 lg:px-6 flex-1 space-y-6">
+        <div className="px-2 lg:px-6 py-2 flex-1 space-y-6">
           <div className="hidden xl:block w-full h-96 bg-gray-300">Image</div>
           <div className="flex space-x-6">
             <div className="w-1/2 lg:w-1/3">
