e49151722c87c6b82ed18e1b35d309cb02c73b9f
difaananda40 <difacool40@gmail.com>
2021-03-05 23:11:27 +0800

    Fix spacing

--- src/components/Home/Recent.jsx
+++ src/components/Home/Recent.jsx
@@ -18,7 +18,7 @@ const Recent = ({ id }) => {
       <div className="flex flex-col xl:flex-row">
         <div className="relative px-2 lg:px-6 py-4 w-full xl:w-1/3">
           <BadgeNumber number="02" />
-          <h1 className="font-black text-3xl sm:text-4xl md:text-5xl xl:text-6xl mb-8 xl:mb-28 uppercase">MOST RECENT NEWS HEADLINE</h1>
+          <h1 className="font-black text-3xl sm:text-4xl md:text-5xl xl:text-6xl mb-8 xl:mb-28 uppercase tracking-wide">MOST RECENT NEWS HEADLINE</h1>
           <div className="xl:hidden md:block w-full h-96 mb-8 xl:mb-0 bg-gray-300">Image</div>
           <div className="flex flex-col md:flex-row xl:flex-col md:space-x-8 items-start justify-start xl:space-x-0 space-y-8 md:space-y-0 xl:space-y-8">
             <p className="font-mono sm:text-lg xl:text-xl text-gray-600">Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum adipisci, voluptates excepturi molestias dolorum ducimus soluta cupiditate possimus nobis quam dicta sint illo doloremque pariatur quae, quia aut, officiis animi.</p>
