d8be806758caf4e45f2a524f1fc3857caa4f273c
difaananda40 <difacool40@gmail.com>
2021-03-06 23:44:03 +0800

    Fix spacing

--- src/components/shared/Navbar/NavbarBlog.jsx
+++ src/components/shared/Navbar/NavbarBlog.jsx
@@ -15,7 +15,7 @@ const NavbarBlog = ({ title }) => {
       </AniLink>
       <div className="lg:px-6 block w-full">
         <h1 className="lg:hidden font-black text-3xl sm:text-4xl md:text-5xl xl:text-6xl mb-4 uppercase">{title}</h1>
-        <div className="flex lg:flex-col">
+        <div className="flex lg:flex-col space-x-6">
           <h2 className="uppercase w-1/2 lg:w-auto font-light mb-6">Categories</h2>
           <ul className="lg:ml-5 w-1/2 lg:w-auto lg:space-y-1 flex flex-wrap lg:flex-col">
             <li className="uppercase font-light">News</li>
