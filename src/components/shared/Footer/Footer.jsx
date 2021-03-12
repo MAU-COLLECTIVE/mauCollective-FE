a1f5c0885d74ed190dd5a2c439b92b7fb1b32d2e
difaananda40 <difacool40@gmail.com>
2021-03-13 03:21:00 +0800

    Adjust spacing

--- src/components/shared/Footer/Footer.jsx
+++ src/components/shared/Footer/Footer.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 
 const Footer = () => (
-  <footer className="px-2 lg:px-12 py-12 flex flex-col space-y-4 xl:space-y-0 xl:flex-row">
+  <footer className="px-4 lg:px-12 py-12 flex flex-col space-y-4 xl:space-y-0 xl:flex-row">
     <div className="xl:w-1/3 flex justify-between xl:justify-start">
       <h1 className="font-light text-sm">M.A.U Collective</h1>
       <div className="flex justify-end space-x-2 xl:hidden">
@@ -16,11 +16,11 @@ const Footer = () => (
         <p className="font-light text-sm">Ho Chi Minh City</p>
         <p className="font-light text-sm">Vietnam</p>
       </div>
-      <div className="w-1/2 sm:w-auto">
+      <div className="w-1/2 pl-4 sm:pl-0 sm:w-auto">
         <p className="font-light text-sm">371/4 Hai Ba Trung</p>
         <p className="font-light text-sm">District 3</p>
       </div>
-      <div className="w-full sm:w-auto flex sm:flex-col mt-4 sm:mt-0">
+      <div className="w-full sm:w-auto flex sm:flex-col mt-4 sm:mt-0 space-x-8 sm:space-x-0">
         <p className="font-light text-sm w-1/2 sm:w-auto">
           <span className="font-medium md:hidden mr-2 text-gray-400">T</span>
           +84 93 785 10 36
