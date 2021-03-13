72b2de405a728dd623bcd183ed5c8d5d07abd400
difaananda40 <difacool40@gmail.com>
2021-03-13 10:10:17 +0800

    Add some spacing

--- src/components/shared/Navbar/NavbarHome.jsx
+++ src/components/shared/Navbar/NavbarHome.jsx
@@ -17,37 +17,37 @@ const NavComponent = ({ className }) => {
     <div className={`lg:space-x-4 ${className}`}>
       <div className="relative w-full lg:w-auto lg:px-6 py-2 lg:py-4">
         <BadgeNumber number="01" className="hidden lg:block" />
-        <a href="#" onClick={(e) => { e.preventDefault(); scrollTo(0, 0); }} className="break-words block font-normal text-xs lg:font-medium uppercase">Home</a>
+        <a href="#" onClick={(e) => { e.preventDefault(); scrollTo(0, 0); }} className="break-words block font-normal text-xs tracking-widest lg:font-medium uppercase">Home</a>
       </div>
       <div className="relative w-full lg:w-auto lg:px-6 py-2 lg:py-4">
         <BadgeNumber number="02" className="hidden lg:block" />
-        <a href="#news" className="break-words block font-normal text-xs lg:font-medium uppercase">News</a>
+        <a href="#news" className="break-words block font-normal text-xs tracking-widest lg:font-medium uppercase">News</a>
       </div>
       <div className="relative w-full lg:w-auto lg:px-6 py-2 lg:py-4">
         <BadgeNumber number="03" className="hidden lg:block" />
-        <a href="#about" className="break-words block font-normal text-xs lg:font-medium uppercase">About M.A.U</a>
+        <a href="#about" className="break-words block font-normal text-xs tracking-widest lg:font-medium uppercase">About M.A.U</a>
       </div>
       <div className="relative w-full lg:w-auto lg:px-6 py-2 lg:py-4">
         <BadgeNumber number="04" className="hidden lg:block" />
-        <a href="#artists" className="break-words block font-normal text-xs lg:font-medium uppercase">Artists</a>
+        <a href="#artists" className="break-words block font-normal text-xs tracking-widest lg:font-medium uppercase">Artists</a>
         <button
           id="btn-artist-type"
-          className="absolute w-max break-words hidden font-normal text-xs lg:font-medium uppercase focus:outline-none"
+          className="absolute w-max break-words hidden font-normal text-xs tracking-widest lg:font-medium uppercase focus:outline-none"
           onClick={toggleArtistType}>
           {ctx.artistType === 'slider' ? 'View all artist' : 'Shuffle'}
         </button>
       </div>
       <div className="relative w-full lg:w-auto lg:px-6 py-2 lg:py-4">
         <BadgeNumber number="05" className="hidden lg:block" />
-        <a href="#collaborations" className="break-words block font-normal text-xs lg:font-medium uppercase">Collaborations</a>
+        <a href="#collaborations" className="break-words block font-normal text-xs tracking-widest lg:font-medium uppercase">Collaborations</a>
       </div>
       <div className="relative w-full lg:w-auto lg:px-6 py-2 lg:py-4">
         <BadgeNumber number="06" className="hidden lg:block" />
-        <a href="#events" className="break-words block font-normal text-xs lg:font-medium uppercase">Events</a>
+        <a href="#events" className="break-words block font-normal text-xs tracking-widest lg:font-medium uppercase">Events</a>
       </div>
       <div className="relative w-full lg:w-auto lg:px-6 py-2 lg:py-4">
         <BadgeNumber number="07" className="hidden lg:block" />
-        <a href="#" className="break-words block font-normal text-xs lg:font-medium uppercase">Shop</a>
+        <a href="#" className="break-words block font-normal text-xs tracking-widest lg:font-medium uppercase">Shop</a>
       </div>
     </div>
   )
@@ -115,14 +115,14 @@ const NavbarHome = () => {
         style={{ mixBlendMode: 'exclusion', filter: 'invert(1)' }}>
         <NavComponent className="hidden lg:flex flex-wrap flex-col justify-start items-start lg:flex-row" />
         <button
-          className="block lg:hidden font-medium uppercase py-4"
+          className="block lg:hidden font-medium uppercase text-xs tracking-widest py-4"
           onClick={handleToggle}>MENU</button>
         <div className="lg:px-6 py-4 flex lg:flex-1 justify-end space-x-2 lg:mr-2">
-          <p className="font-medium uppercase">EN</p>
-          <span>/</span>
-          <p className="font-medium uppercase">VN</p>
-          <span>/</span>
-          <p className="font-medium uppercase">JP</p>
+          <p className="font-medium uppercase text-xs tracking-widest">EN</p>
+          <span className="text-xs">/</span>
+          <p className="font-medium uppercase text-xs tracking-widest">VN</p>
+          <span className="text-xs">/</span>
+          <p className="font-medium uppercase text-xs tracking-widest">JP</p>
         </div>
       </nav>
       {/* Sidebar */}
@@ -134,11 +134,11 @@ const NavbarHome = () => {
         </button>
         <NavComponent />
         <div className="lg:px-6 py-2 lg:py-4 flex lg:flex-1 justify-end space-x-2 mt-auto mb-8">
-          <p className="font-medium uppercase">EN</p>
-          <span>/</span>
-          <p className="font-medium uppercase">VN</p>
-          <span>/</span>
-          <p className="font-medium uppercase">JP</p>
+          <p className="font-medium uppercase text-xs tracking-widest">EN</p>
+          <span className="text-xs">/</span>
+          <p className="font-medium uppercase text-xs tracking-widest">VN</p>
+          <span className="text-xs">/</span>
+          <p className="font-medium uppercase text-xs tracking-widest">JP</p>
         </div>
       </div>
     </React.Fragment>
