7271414bc5e2b5854664cd59b9a538dae36ad949
difaananda40 <difacool40@gmail.com>
2021-03-21 00:50:38 +0800

    Rework layout

--- src/components/Home/About.jsx
+++ src/components/Home/About.jsx
@@ -8,8 +8,8 @@ const About = ({ id }) => {
 
   return (
     <div id={id} className="min-h-screen bg-black text-white px-2 lg:px-6 py-28 flex flex-col justify-center space-y-20">
-      <div className="flex flex-col lg:flex-row space-y-20 lg:space-y-0">
-        <div className="relative lg:w-1/2 px-2 lg:px-6 py-4">
+      <div className="flex flex-col xl:flex-row space-y-20 xl:space-y-0">
+        <div className="relative xl:w-3/5 px-2 lg:px-6 py-4">
           <BadgeNumber number="03" />
           <h1 className="font-semibold text-3xl mb-4 text-white uppercase">{t('aboutSection.aboutCategory')}</h1>
           <p className="font-mono xl:text-lg text-gray-200">Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum adipisci, voluptates excepturi molestias dolorum ducimus soluta cupiditate possimus nobis quam dicta sint illo doloremque pariatur quae, quia aut, officiis animi.
@@ -26,26 +26,28 @@ const About = ({ id }) => {
           </div>
         </div>
       </div>
-      <div className="relative px-2 lg:px-6 py-4">
+      <div className="relative px-2 lg:px-6 py-4 xl:w-3/5">
         <h1 className="font-semibold text-3xl mb-8 text-white uppercase">{t('aboutSection.contactUsField')}</h1>
-        <div className="flex sm:space-x-28 space-y-8 sm:space-y-0 flex-col sm:flex-row">
-          <div>
+        <div className="flex flex-wrap space-y-8 sm:space-y-0 justify-between">
+          <div className="w-full sm:w-auto">
             <p className="font-mono xl:text-lg text-gray-200">M.A.U Collective</p>
           </div>
-          <div className="flex space-x-14 sm:space-x-28">
+          <div className="flex flex-col lg:flex-row xl:flex-col space-x-0 lg:space-x-28 xl:space-x-0">
             <div>
               <p className="font-mono xl:text-lg text-gray-200">371/4 Hai Ba Trung</p>
               <p className="font-mono xl:text-lg text-gray-200 mb-6">District 3</p>
+            </div>
+            <div>
               <p className="font-mono xl:text-lg text-gray-200">Ho Chi Minh City</p>
               <p className="font-mono xl:text-lg text-gray-200">Vietnam</p>
             </div>
+          </div>
+          <div className="flex flex-col md:flex-row xl:flex-col 2xl:flex-row md:space-x-28 2xl:space-x-28 xl:space-x-0">
             <div>
               <p className="font-mono xl:text-lg text-gray-200">Email</p>
               <p className="font-mono xl:text-lg text-gray-200 mb-6">info@mau.network</p>
-              <p className="font-mono xl:text-lg text-gray-200 block md:hidden">Phone</p>
-              <p className="font-mono xl:text-lg text-gray-200 block md:hidden">+84 93 785 10 36</p>
             </div>
-            <div className="hidden md:block">
+            <div>
               <p className="font-mono xl:text-lg text-gray-200">Phone</p>
               <p className="font-mono xl:text-lg text-gray-200">+84 93 785 10 36</p>
             </div>
