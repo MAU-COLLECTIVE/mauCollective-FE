ebffa010f94b203f5b9550b8a3f0e77866a41a62
difaananda40 <difacool40@gmail.com>
2021-04-07 04:10:18 +0800

    Remove smoothscroll polyfill

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -2,9 +2,7 @@ import './src/css/main.css'
 import './src/css/fonts.css'
 
 import React from 'react';
-import smoothscroll from 'smoothscroll-polyfill';
 import { HomeProvider } from './src/contexts/HomeContext';
-smoothscroll.polyfill();
 
 export const wrapRootElement = ({ element }) => (
   <HomeProvider>
