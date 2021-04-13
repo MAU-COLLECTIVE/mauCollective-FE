9d7bde9571fd7efea27e1fae75fb974b09f3f145
difaananda40 <difacool40@gmail.com>
2021-04-14 03:30:08 +0800

    Fix router for close button

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -9,3 +9,8 @@ export const wrapRootElement = ({ element }) => (
     {element}
   </HomeProvider>
 )
+
+export const onRouteUpdate = ({ location, prevLocation }) => {
+  if (location && location.state)
+    location.state.referrer = prevLocation ? prevLocation.pathname : null
+}
