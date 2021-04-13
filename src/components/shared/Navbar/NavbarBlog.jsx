db037c8d2806af47664985c0dc5bad7635b7549d
difaananda40 <difacool40@gmail.com>
2021-04-14 03:30:08 +0800

    Fix router for close button

--- src/components/shared/Navbar/NavbarBlog.jsx
+++ src/components/shared/Navbar/NavbarBlog.jsx
@@ -1,15 +1,17 @@
 import React from 'react'
 import OverlayLink from 'components/shared/OverlayLink'
 import { useTranslation } from 'gatsby-plugin-react-i18next'
+import { useLocation } from '@reach/router'
 
 const NavbarBlog = () => {
   const { t } = useTranslation();
+  const location = useLocation();
 
   return (
     <nav className="lg:h-screen p-2 lg:p-10 text-black flex flex-col items-start">
       <OverlayLink
         type="main"
-        to="/"
+        to={location?.state?.referrer ?? '/'}
         className="relative p-4 mb-8 lg:mb-16">
           <span className="block font-medium uppercase text-xs">{t('shared.close')}</span>
       </OverlayLink>
