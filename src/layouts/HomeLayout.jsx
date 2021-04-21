ce2b43bfa0d0c05fde7c7ca917649c8eda2c2154
BaoNam99 <obamavn99@gmail.com>
2021-04-21 23:00:15 +0700

    Update 404 page and shop page

--- src/layouts/HomeLayout.jsx
+++ src/layouts/HomeLayout.jsx
@@ -5,22 +5,21 @@ import PropTypes from 'prop-types'
 import { NavbarHome } from 'components/shared/Navbar'
 import Footer from 'components/shared/Footer'
 
-const HomeLayout = ({ children }) => {
-  return (
-    <React.Fragment>
-      <header>
-        <NavbarHome />
-      </header>
-      <main>
-        {children}
-      </main>
-      <Footer />
-    </React.Fragment>
-  )
+const HomeLayout = ({ children, hideFooter }) => {
+	return (
+		<React.Fragment>
+			<header>
+				<NavbarHome />
+			</header>
+			<main>{children}</main>
+			{!hideFooter && <Footer />}
+		</React.Fragment>
+	)
 }
 
 HomeLayout.propTypes = {
-  children: PropTypes.node
+	children: PropTypes.node,
+	hideFooter: PropTypes.bool,
 }
 
 export default HomeLayout
---
+++ src/pages/404.jsx
@@ -0,0 +1,116 @@
+import React from 'react'
+import { graphql } from 'gatsby'
+import PropTypes from 'prop-types'
+
+import HomeLayout from 'layouts/HomeLayout'
+import SEO from 'components/shared/SEO'
+import OverlayLink from '../components/shared/OverlayLink'
+
+export const query = graphql`
+	query NotFoundPage($language: String!) {
+		locales: allLocale(filter: { language: { eq: $language } }) {
+			edges {
+				node {
+					ns
+					data
+					language
+				}
+			}
+		}
+		# Get about
+		sanityAbout {
+			name
+			aboutTab {
+				email
+				phoneNum
+				logo {
+					asset {
+						id
+						gatsbyImageData
+					}
+				}
+				description {
+					lang {
+						en
+						jp
+						vn
+					}
+				}
+				metaSeoDescription {
+					lang {
+						en
+						jp
+						vn
+					}
+				}
+				social {
+					facebook
+					instagram
+					spotify
+					youtube
+				}
+				address {
+					city {
+						lang {
+							en
+							jp
+							vn
+						}
+					}
+					country {
+						lang {
+							en
+							jp
+							vn
+						}
+					}
+					streetName {
+						lang {
+							en
+							jp
+							vn
+						}
+					}
+					streetNo {
+						lang {
+							en
+							jp
+							vn
+						}
+					}
+				}
+			}
+		}
+	}
+`
+
+const NotFoundPage = ({ pageContext, data }) => {
+	const { language } = pageContext
+	const { sanityAbout: about } = data
+
+	return (
+		<HomeLayout hideFooter={true}>
+			<SEO
+				description={about?.aboutTab?.metaSeoDescription?.lang?.[language]}
+			/>
+			<div className="h-screen bg-black text-white flex flex-col text-center justify-center items-center uppercase ">
+				<div className="text-2xl md:text-3xl lg:text-4xl font-bold mb-3">
+					404 page not found
+				</div>
+				<OverlayLink
+					type="secondary"
+					to={'/'}
+					className="whitespace-nowrap text-base border-solid border-white border-b-2 hover:text-gray-600 hover:border-gray-600 transition-colors">
+					go back to home page
+				</OverlayLink>
+			</div>
+		</HomeLayout>
+	)
+}
+
+NotFoundPage.propTypes = {
+	pageContext: PropTypes.object.isRequired,
+	data: PropTypes.object.isRequired,
+}
+
+export default React.memo(NotFoundPage)
