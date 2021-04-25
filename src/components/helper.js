e61292dd9cea52dc3cf669b41061184e0f269ba9
difaananda40 <difacool40@gmail.com>
2021-04-26 04:08:55 +0800

    Add youtube on block-content

--- src/components/helper.js
+++ src/components/helper.js
@@ -7,3 +7,22 @@ export const getGatsbyImage = (data, params) => {
 };
 
 export const capitalize = string => string.trim().replace(/^\w/, (c) => c.toUpperCase());
+
+export const getVideoEmbed = (input) => {
+  if (input.match( /youtu/ )) {
+    const pattern = /(?:http?s?:\/\/)?(?:www\.)?(?:youtube\.com|youtu\.be)\/(?:watch\?v=)?(\S+)/g;
+    if (pattern.test(input)) {
+      let newInput = input;
+      const replacement = '<iframe width="560" height="349" src="https://www.youtube.com/embed/$1" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen loading="lazy"></iframe>';
+      return newInput.replace(pattern, replacement);
+    }
+  }
+  else if (input.match( /vimeo/ )) {
+    const pattern = /(?:http?s?:\/\/)?(?:www\.)?(?:vimeo\.com)\/?(\S+)/g;
+    if (pattern.test(input)) {
+      let newInput = input;
+      const replacement = '<iframe width="560" height="349" src="//player.vimeo.com/video/$1" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen loading="lazy"></iframe>';
+      return newInput.replace(pattern, replacement);
+    }
+  }
+}
